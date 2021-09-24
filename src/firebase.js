d06348896f9e9ff52404761d7ae3b1cdfb5f8db3
vikas <vikaspatelkherad@gmail.com>
2021-09-24 04:10:30 -0700

    S1 - Structure of WC

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2lYPKt_qcm-mBpGqk-OMGX8jIKcYOgI4",
+  authDomain: "whats-app-clone-4601f.firebaseapp.com",
+  projectId: "whats-app-clone-4601f",
+  storageBucket: "whats-app-clone-4601f.appspot.com",
+  messagingSenderId: "623415363787",
+  appId: "1:623415363787:web:7ed7039c791aedce03334e",
+  measurementId: "G-GT5MMLNQ9X",
+};
