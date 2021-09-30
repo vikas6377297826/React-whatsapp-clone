928d3922135c51d263bc48da5f8ceee1a32d1279
vikas <vikaspatelkherad@gmail.com>
2021-09-30 00:16:56 -0700

    creating sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,43 @@
+import { IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { Avatar } from "@mui/material";
 import React from "react";
+import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
-      <div className="sidebar__search"></div>
-      <div className="sidebar__chats"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+      </div>
     </div>
   );
 };
