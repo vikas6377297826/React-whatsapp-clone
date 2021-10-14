614a64e626e05fd4a7bc1d21ed2f712456a55933
vikas <vikaspatelkherad@gmail.com>
2021-10-14 07:11:14 -0700

    shows messgae based on room

--- src/Chat.js
+++ src/Chat.js
@@ -10,6 +10,7 @@ import {
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import "./Chat.css";
+import { db } from "./firebase";
 
 const Chat = () => {
   const [input, setInput] = useState("");
@@ -17,6 +18,16 @@ const Chat = () => {
   const [seed, setSeed] = useState("");
 
   const { roomId } = useParams();
+
+  const [roomName, setRoomName] = useState("");
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+    }
+  }, [roomId]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -32,7 +43,7 @@ const Chat = () => {
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Room name</h3>
+          <h3>{roomName}</h3>
           <p>Last seen at ...</p>
         </div>
         <div className="chat__headerRight">
